f758cc9ed0421dd6b8419e1449fc9da9b7e0da0d
AlexM <alexmedearis@gmail.com>
2026-08-22 20:44:14 -0700

    Initial Commit

---
+++ Touch Demo/Touch_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Touch_DemoApp.swift
+//  Touch Demo
+//
+//  Created by Carl Allen on 8/22/26.
+//
+
+import SwiftUI
+
+@main
+struct Touch_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
